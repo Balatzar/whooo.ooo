f3052facd86a841513733a423a3686880d3bb47c
Tristan Chapelle <Tristan@MacBook-Pro-de-Tristan.local>
2017-01-22 18:04:13 +0100

    create / create party

--- imports/ui/components/header/header.js
+++ imports/ui/components/header/header.js
@@ -2,6 +2,8 @@ import { Meteor } from 'meteor/meteor'
 import { Template } from 'meteor/templating'
 import { Session } from 'meteor/session'
 
+import '../burd/burd.js'
+
 import './header.html';
 import './header.css';
 
