630e4ab5c5b7d75a4097d213a18c03757991e354
balatzar <bataprod@gmail.com>
2017-01-22 18:17:22 +0100

    playlist

--- imports/ui/components/header/header.js
+++ imports/ui/components/header/header.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor'
 import { Template } from 'meteor/templating'
 import { Session } from 'meteor/session'
+import { FlowRouter } from 'meteor/kadira:flow-router'
 
 import './header.html';
 import './header.css';
@@ -8,5 +9,13 @@ import './header.css';
 Template.header.helpers({
   getUsername() {
     return Session.get('username')
+  }
+})
+
+Template.header.events({
+  'click .deco'() {
+    Session.set('username', null)
+    FlowRouter.go('indexPage')
+    location.reload()
   }
 })
--- imports/ui/layouts/party/party.js
+++ imports/ui/layouts/party/party.js
@@ -3,12 +3,14 @@ import { Template } from 'meteor/templating'
 import { FlowRouter } from 'meteor/kadira:flow-router'
 
 import Party from '../../../api/party/party'
+import Song from '../../../api/song/song'
 
 import './party.css'
 import './party.html'
 import '../../components/youtubeplayer/youtubeplayer.js'
 import '../../components/addsongform/addsongform.js'
 import '../../components/burd/burd.js'
+import '../../components/playlist/playlist.js'
 
 Template.party.onRendered(() => {
   Meteor.subscribe('parties.all')
@@ -22,5 +24,5 @@ Template.party.helpers({
   burds() {
     const party = Party.findOne(FlowRouter.current().params.id)
     return party ? party.burds : []
-  }
+  },
 })
