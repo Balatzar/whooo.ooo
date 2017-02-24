43f8437ea09d7d3053a7ac0afdf2190880c01b7f
balatzar <bataprod@gmail.com>
2017-02-24 20:22:46 +0100

    fix bugs

--- imports/ui/components/header/header.js
+++ imports/ui/components/header/header.js
@@ -11,11 +11,13 @@ Template.header.helpers({
   getUser() {
     return Meteor.user() || false
   },
-  isMobile: function () {
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent) || width < 900){
-      return true 
-    }else{
-      return false 
+
+  isMobile() {
+    const width = $('body').width()
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent) || width < 900) {
+      return true
+    } else {
+      return false
     }
   }
 })
