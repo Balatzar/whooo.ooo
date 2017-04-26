0228dc25ab8f7bc7ea60407790462d8e7818d404
balatzar <bataprod@gmail.com>
2017-04-26 11:24:14 +0200

    add vote

---
+++ imports/api/songParty/methods.js
@@ -0,0 +1,29 @@
+import { Meteor } from "meteor/meteor"
+import { check } from "meteor/check"
+
+import SongParty from "../songParty/songParty.js"
+
+Meteor.methods({
+  "songParty.vote"(_id) {
+    check(_id, String)
+    const song = SongParty.findOne(_id)
+    if (song.votes.indexOf(this.userId) !== -1) {
+      return SongParty.update(
+        { _id },
+        {
+          $pop: {
+            votes: this.userId,
+          },
+        }
+      )
+    }
+    return SongParty.update(
+      { _id },
+      {
+        $push: {
+          votes: this.userId,
+        },
+      }
+    )
+  },
+})
--- imports/ui/components/song/song.js
+++ imports/ui/components/song/song.js
@@ -1,2 +1,17 @@
+import { Meteor } from "meteor/meteor"
+import { Template } from "meteor/templating"
+
 import "./song.html"
 import "./song.css"
+
+Template.song.events({
+  "click .js-vote"() {
+    Meteor.call("songParty.vote", this._id, (err, res) => {
+      if (err) {
+        console.warn(err)
+      } else {
+        console.log(res)
+      }
+    })
+  },
+})
